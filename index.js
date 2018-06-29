abf03e0e8a8e60a2243397203f8b62ffe3e324b5
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-06-29 11:21:47 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+ES6: Compare Scopes of the var and let Keywords
+
+1)
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+
+2)
